f41baafeeeb91cadec5c6dcab4afe25884f436aa
Tasneem Ibrahim <tasneem@example.com>
2025-07-27 18:01:45 +0400

    Add solution for warmup1-front22

--- front22.java
+++ front22.java
@@ -3,3 +3,11 @@ public String front22(String str) {
     return front + str + front;
 }
 
+  public static void main(String[] args) {
+        Front22 f = new Front22();
+        System.out.println(f.front22("kitten")); // kikittenki
+        System.out.println(f.front22("Ha"));     // HaHaHa
+        System.out.println(f.front22("abc"));    // ababcab
+        System.out.println(f.front22("x"));      // xxx
+    }
+}
