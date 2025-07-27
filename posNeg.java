be4f00805930f55d8437e1f337c74948d1a60fa2
Tasneem Ibrahim <tasneem@example.com>
2025-07-27 14:20:32 +0400

    Add solution for warmup1-posNeg

--- posNeg.java
+++ posNeg.java
@@ -1,8 +1,17 @@
-public boolean posNeg(int a, int b, boolean negative) {
-    if (negative) {
-        return a < 0 && b < 0;
-    } else {
-        return (a < 0 && b > 0) || (a > 0 && b < 0);
+public class PosNeg {
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return a < 0 && b < 0;
+        } else {
+            return (a < 0 && b > 0) || (a > 0 && b < 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        PosNeg p = new PosNeg();
+        System.out.println(p.posNeg(1, -1, false));  // true
+        System.out.println(p.posNeg(-1, 1, false));  // true
+        System.out.println(p.posNeg(-4, -5, true));  // true
     }
 }
 
