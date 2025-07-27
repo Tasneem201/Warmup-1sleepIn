4bcd0214ef8b2e935d2e7a476f74d842a97ed4b2
Tasneem Ibrahim <tasneem@example.com>
2025-07-27 17:58:32 +0400

    Add solution for warmup1-or35

--- or35.java
+++ or35.java
@@ -1,4 +1,11 @@
 public boolean or35(int n) {
     return n % 3 == 0 || n % 5 == 0;
 }
-
+ public static void main(String[] args) {
+        Or35 o = new Or35();
+        System.out.println(o.or35(3));   // true
+        System.out.println(o.or35(10));  // true
+        System.out.println(o.or35(8));   // false
+        System.out.println(o.or35(0));   // true
+    }
+}
