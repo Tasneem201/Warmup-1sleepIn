b068f280790187fce4e430eebfe03d22c7394b7d
Tasneem Ibrahim <tasneem@example.com>
2025-07-27 18:06:03 +0400

    Add solution for warmup1-startHi

--- startHi.java
+++ startHi.java
@@ -1,3 +1,13 @@
-public boolean startHi(String str) {
-    return str.startsWith("hi");
+public class StartHi {
+    public boolean startHi(String str) {
+        return str.startsWith("hi");
+    }
+
+    public static void main(String[] args) {
+        StartHi s = new StartHi();
+        System.out.println(s.startHi("hi there"));   // true
+        System.out.println(s.startHi("hi"));         // true
+        System.out.println(s.startHi("hello hi"));   // false
+    }
 }
+
