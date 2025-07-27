66842dc5c0cd2f92a737a38996e99361d741e4d4
Tasneem Ibrahim <tasneem@example.com>
2025-07-27 19:22:49 +0400

    Add solution for warmup1-mixStart

--- mixStart.java
+++ mixStart.java
@@ -1,4 +1,13 @@
-public boolean mixStart(String str) {
-    return str.length() >= 3 && str.substring(1, 3).equals("ix");
+public class MixStart {
+    public boolean mixStart(String str) {
+        return str.length() >= 3 && str.substring(1, 3).equals("ix");
+    }
+
+    public static void main(String[] args) {
+        MixStart ms = new MixStart();
+        System.out.println(ms.mixStart("mix snacks")); // true
+        System.out.println(ms.mixStart("pix snacks")); // true
+        System.out.println(ms.mixStart("piz snacks")); // false
+    }
 }
 
