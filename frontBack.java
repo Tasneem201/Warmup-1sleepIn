97174d6828ed8a83caac0605fab536f3c0b649b6
Tasneem Ibrahim <tasneem@example.com>
2025-07-27 17:43:27 +0400

    Add solution for warmup1-frontBack

--- frontBack.java
+++ frontBack.java
@@ -6,3 +6,10 @@ public String frontBack(String str) {
     String mid = str.substring(1, str.length() - 1);
     return str.charAt(str.length() - 1) + mid + str.charAt(0);
 }
+public static void main(String[] args) {
+        FrontBack fb = new FrontBack();
+        System.out.println(fb.frontBack("code")); // eodc
+        System.out.println(fb.frontBack("a"));    // a
+        System.out.println(fb.frontBack("ab"));   // ba
+    }
+}
