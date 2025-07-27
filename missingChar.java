005776b19db512bd3eef676a64ae36e4d3a28c2f
Tasneem Ibrahim <tasneem@example.com>
2025-07-27 17:34:53 +0400

    Add solution for warmup1-missingChar

--- missingChar.java
+++ missingChar.java
@@ -1,3 +1,15 @@
+public class MissingChar {
+    public String missingChar(String str, int n) {
+        return str.substring(0, n) + str.substring(n + 1);
+    }
+
+    public static void main(String[] args) {
+        MissingChar mc = new MissingChar();
+        System.out.println(mc.missingChar("kitten", 1)); // ktten
+        System.out.println(mc.missingChar("kitten", 0)); // itten
+        System.out.println(mc.missingChar("kitten", 4)); // kittn
+    }
+}
 public String missingChar(String str, int n) {
     return str.substring(0, n) + str.substring(n + 1);
 }
